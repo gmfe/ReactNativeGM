a63c1f23ca10523bc72aab7b8f67f8c92364e706
zhangsiqi <276987359@qq.com>
2016-10-27 17:20:07 +0800

    TabsItem加数字红点

--- src/tabs/index.js
+++ src/tabs/index.js
@@ -1,26 +1,50 @@
 import React, {PropTypes} from 'react';
-import {View, TouchableHighlight} from 'react-native';
+import {View, TouchableHighlight, StyleSheet} from 'react-native';
 import {Text} from '../typography';
 import S from '../styles';
 import V from '../variable';
 import _ from 'underscore';
 
+const styles = StyleSheet.create({
+    redPoint: {
+        position: 'absolute',
+        top: 4,
+        right: -2,
+        height: 18,
+        width: 18,
+        backgroundColor: 'red',
+        borderRadius: 9,
+        overflow: 'hidden'
+    },
+    redCount: {
+        color: 'white',
+        fontSize: 12
+    }
+});
+
+
 class TabsItem extends React.Component {
     render() {
-        const {children, active, onPress} = this.props;
+        const {children, redCount, active, onPress} = this.props;
         return (
             <TouchableHighlight
                 style={[S.flex, S.flexAlignCenter]}
                 underlayColor={V.activeColor}
                 onPress={onPress}
             >
-                <View style={active ? {
+                <View style={[S.flexRow, active ? {
                     borderBottomWidth: 2,
                     borderBottomColor: V.primaryColor
-                } : {}}>
+                } : {}]}>
                     <Text
                         style={[S.paddingTop10, S.paddingBottom10, S.paddingLeft15, S.paddingRight15, active && S.textPrimary]}
                     >{children}</Text>
+                    {
+                        redCount ? <View style={[S.flexJustifyCenter, S.flexAlignCenter, styles.redPoint]}>
+                            <Text style={styles.redCount}>{redCount}</Text>
+                        </View>
+                            : null
+                    }
                 </View>
             </TouchableHighlight>
         );
